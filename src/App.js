377c0b4578b73ccde69afa60943b4271a0c34b6a
Shivam Kumar <119694506+kumarshivam04203@users.noreply.github.com>
2023-12-01 20:45:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+import './index.css';
+// import products from "./data/data";
+
+// database
+import products from './db/data.js'
+import Card from "./components/Card";
+ 
+function App() {
+
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState("");
+
+  //-------------- Input filter-----------
+
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value)
+  };
+
+  const filteredItems = products.filter((product) => 
+  product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+
+  //------------- radio filter----------
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value)
+  }
+
+  //------------ Button filter----------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value)
+  }
+
+  function filteredData(products, selected, query){
+    let filteredProducts = products;
+     
+    // filtering input items
+    if(query){
+      filteredProducts = filteredItems;
+    }
+
+    //Selected filter
+    if(selected){
+      filteredProducts = filteredProducts.filter(
+        ({category, color, company, newPrice, title}) => 
+        category === selected || color === selected || company === selected || newPrice === selected || title === selected
+      );
+    }
+
+    return filteredProducts.map(({img, title, star, reviews,newPrice, prevPrice}) => (
+      <Card
+      key={Math.random()}
+      img={img}
+      title={title}
+      star={star}
+      reviews={reviews}
+      newPrice={newPrice}
+      prevPrice={prevPrice}
+      />
+    ))
+     
+  }
+  const result = filteredData(products, selectedCategory, query)
+
+
+  return (
+    <>
+      <Sidebar handleChange={handleChange}/>
+      <Navigation query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
+      <Products result={result} />
+    </>
+  );
+}
+
+export default App;
